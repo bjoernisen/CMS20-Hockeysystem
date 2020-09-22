46e45df411d4273161aa9b40a203a6977ae10d69
bjoernisen <b.lindebeerg@hotmail.com>
2020-09-22 12:39:50 +0200

    Add project files.

---
+++ CMS20 Hockeysystem/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CMS20_Hockeysystem
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int JesreyNumber { get; set; }
+    }
+}
---
+++ CMS20 Hockeysystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMS20_Hockeysystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
